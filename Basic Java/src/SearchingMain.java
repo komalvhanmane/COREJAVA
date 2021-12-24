f8ba573f3fb3a68552bea2cef6a2a78a1d677564
komal <vhanmanekomal120603@gmail.com>
2021-12-24 06:34:13 +0530

    searching logic basic

--- Basic Java/src/SearchingMain.java
+++ Basic Java/src/SearchingMain.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class SearchingMain {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+      /*  Scanner sc=new Scanner(System.in);
         Searching s=new Searching();
         System.out.println("Enter the number of total elements ");
         int n=sc.nextInt();
@@ -11,7 +11,35 @@ public class SearchingMain {
         for(int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
         }
-        s.binarySearch(arr,5);
-       // s.linearSearch(arr,3);
+        System.out.println("Enter choice \n1-Linear search \n2- Binary Search");*/
+      /*  String str="I am komal";
+        StringBuilder ans= new StringBuilder("" + (str.charAt(0)));
+        ans = new StringBuilder(ans.toString().toUpperCase());
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i-1)==' '){
+                String temp=str.charAt(i)+"";
+                ans.append(temp.toUpperCase());
+            }
+            else{
+                ans.append(str.charAt(i));
+            }
+        }
+        int i=3;
+        System.out.println(ans);*/
+        String message="aaaabbbccd";
+        StringBuilder ans= new StringBuilder(message.charAt(0) + "");
+        int c=1;
+        for(int i=1;i<message.length();i++){
+            if(message.charAt(i-1)==message.charAt(i)){
+                c++;
+            }
+            else{
+                ans.append(Integer.toString(c));
+                ans.append(message.charAt(i));
+                c=1;
+            }
+        }
+        ans.append(Integer.toString(c));
+        System.out.println(ans);
     }
 }
